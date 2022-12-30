85f9096d7af4e97dcb15fb85e6e3089ab18533e6
Renato Lacerda <renato.lacerda@in8.com.br>
2022-12-30 15:15:54 -0300

    chore: testing routes

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import * as Hapi from '@hapi/hapi';
-import { port, url } from './config'
+import { Server, ResponseToolkit, Request } from 'hapi'
+import { port, url } from './config';
+import 'colors'
 
 const init = async () => {
 
@@ -8,8 +10,18 @@ const init = async () => {
         host: url
     });
 
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request: Request, h: ResponseToolkit, err?: Error) => {
+            return {
+                msg: 'hello World'
+            }
+        }
+    })
+
     await server.start();
-    console.log('Server running on %s', server.info.uri);
+    console.log(`Server running on ${server.info.uri}`.green);
 };
 
 process.on('unhandledRejection', (err) => {
