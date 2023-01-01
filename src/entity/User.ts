e581c233fe41218dae58f87801487f3ddfa363f5
Renato Lacerda <renato.lacerda@in8.com.br>
2023-01-01 17:20:32 -0300

    feat: Create relationship user-vehicle

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,27 +1,33 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm"
 import { SharedProps } from "./SharedProps.js"
+import { Vehicle } from "./Vehicle.js"
 
-export enum UserType{
+export enum UserType {
     client = 'client',
     third = 'third',
 }
 
-@Entity({name: 'user'})
-export class User extends SharedProps{
+@Entity({ name: 'user' })
+export class User extends SharedProps {
 
     @PrimaryGeneratedColumn()
-    cpf: number
+    cpf: number;
 
-    @Column({name: 'first_name', nullable: false})
-    firstName: string
+    @Column({ name: 'first_name', nullable: false })
+    firstName: string;
 
-    @Column({name: 'last_name', nullable: false})
-    lastName: string
+    @Column({ name: 'last_name', nullable: false })
+    lastName: string;
 
-    @Column({name: 'phone', nullable: false})
-    phone: number
+    @Column({ name: 'phone', nullable: false })
+    phone: number;
 
-    @Column({default: UserType.client, enum: UserType, type: 'enum', nullable: false})
-    type: UserType
+    @Column({ default: UserType.client, enum: UserType, type: 'enum', nullable: false })
+    type: UserType;
 
+    @OneToMany(() => Vehicle, (vehicle: Vehicle) => vehicle.id, {
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+    })
+    vehicle: Array<Vehicle>;
 }
--- src/entity/Vehicle.ts
+++ src/entity/Vehicle.ts
@@ -1,17 +1,26 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm'
 import { SharedProps } from './SharedProps.js';
+import { User } from './User.js';
 
-@Entity({name: 'vehicle'})
-export class Vehicle extends SharedProps{
+@Entity({ name: 'vehicle' })
+export class Vehicle extends SharedProps {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({name: 'model', nullable: false})
-    model:string;
+    @Column({ name: 'model', nullable: false })
+    model: string;
 
-    @Column({name:'color', nullable:false})
+    @Column({ name: 'color', nullable: false })
     color: string;
 
-    @Column({name:'license_plate', nullable:false})
+    @Column({ name: 'license_plate', nullable: false })
     licensePlate: string;
+
+    @Column({ name: 'user_cpf', nullable: false})
+    userCpf: number
+
+    @ManyToOne(() => User, (user: User) => user.cpf)
+    @JoinColumn({ name: 'user_cpf' })
+    user: User;
+
 }
