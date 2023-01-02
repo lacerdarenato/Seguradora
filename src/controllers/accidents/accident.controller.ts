c4e40b2b76a92c3e749c2e224c12a01336b95cd4
Renato Lacerda <renato.lacerda@in8.com.br>
2023-01-02 11:04:22 -0300

    feat: create new routes to accident.controllers

--- src/controllers/accidents/accident.controller.ts
+++ src/controllers/accidents/accident.controller.ts
@@ -13,5 +13,39 @@ export const accidentController = () => {
 
             }
         },
+        {
+            method: 'GET',
+            path: '/accidents',
+            handler: (request: Request, h: ResponseToolkit, err?: Error) => {
+                return PostgresDataSource.manager.find(Accident);
+
+            }
+        },
+        {
+            method: 'GET',
+            path: '/accidents/{id}',
+            handler: ({ params: { sinister } }: Request, h: ResponseToolkit, err?: Error) => {
+                return PostgresDataSource.manager.findOneBy(Accident, { sinister: Number(sinister) });
+
+            }
+        },
+        {
+            method: 'POST',
+            path: '/accidents',
+            handler: ({ payload }: Request, h: ResponseToolkit, err?: Error) => {
+                const { sinister, date } = payload as Partial<Accident>;
+                const accident: Partial<Accident> = PostgresDataSource.manager.create(Accident,
+                    { sinister, date }
+                );
+                return PostgresDataSource.manager.save<Partial<Accident>>(accident);
+            }
+        },
+        {
+            method: 'PATCH',
+            path: '/accidents/{sinister}',
+            handler: async ({ params: { sinister }, payload }: Request, h: ResponseToolkit, err?: Error) => {
+                return await PostgresDataSource.manager.update<Partial<Accident>>(Accident, sinister, payload)
+            }
+        },
     ]
 }
