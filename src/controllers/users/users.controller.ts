6ccb32e5b3e5b7eaafcee247fb29f71c8180e064
Renato Lacerda <renato.lacerda@in8.com.br>
2023-01-02 10:53:07 -0300

    feat: create new routes to userController

--- src/controllers/users/users.controller.ts
+++ src/controllers/users/users.controller.ts
@@ -13,5 +13,31 @@ export const userController = () => {
 
             }
         },
+        {
+            method: 'GET',
+            path: '/users/{id}',
+            handler: ({ params: { id } }: Request, h: ResponseToolkit, err?: Error) => {
+                return PostgresDataSource.manager.findOneBy(User, { id: Number(id) });
+
+            }
+        },
+        {
+            method: 'POST',
+            path: '/users',
+            handler: ({ payload }: Request, h: ResponseToolkit, err?: Error) => {
+                const { cpf, firstName, lastName, phone, type } = payload as Partial<User>;
+                const user: Partial<User> = PostgresDataSource.manager.create(User,
+                    { cpf, firstName, lastName, phone, type }
+                );
+                return PostgresDataSource.manager.save<Partial<User>>(user);
+            }
+        },
+        {
+            method: 'PATCH',
+            path: '/users/{id}',
+            handler: async ({ params: { id }, payload }: Request, h: ResponseToolkit, err?: Error) => {
+                return await PostgresDataSource.manager.update<Partial<User>>(User, id, payload)
+            }
+        },
     ]
 }
