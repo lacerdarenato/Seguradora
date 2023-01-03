a313c2adc1e758e5fa9a7fe1e148d3f17ff40f32
Renato Lacerda <renato.lacerda@in8.com.br>
2023-01-02 22:14:03 -0300

    feat: create route to vehicle controller

--- src/controllers/vehicles/vehicle.controller.ts
+++ src/controllers/vehicles/vehicle.controller.ts
@@ -13,5 +13,31 @@ export const vehicleController = () => {
 
             }
         },
+        {
+            method: 'GET',
+            path: '/vehicles/{id}',
+            handler: ({ params: { id } }: Request, h: ResponseToolkit, err?: Error) => {
+                return PostgresDataSource.manager.findOneBy(Vehicle, { id: Number(id) });
+
+            }
+        },
+        {
+            method: 'POST',
+            path: '/vehicles',
+            handler: ({ payload }: Request, h: ResponseToolkit, err?: Error) => {
+                const { model, color, licensePlate, user, event } = payload as Partial<Vehicle>;
+                const vehicle: Partial<Vehicle> = PostgresDataSource.manager.create(Vehicle,
+                    { model, color, licensePlate, user, event }
+                );
+                return PostgresDataSource.manager.save<Partial<Vehicle>>(vehicle);
+            }
+        },
+        {
+            method: 'PATCH',
+            path: '/vehicles/{id}',
+            handler: async ({ params: { id }, payload }: Request, h: ResponseToolkit, err?: Error) => {
+                return await PostgresDataSource.manager.update<Partial<Vehicle>>(Vehicle, id, payload)
+            }
+        },
     ]
 }
