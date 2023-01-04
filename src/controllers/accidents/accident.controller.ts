b737bc1764c08892330bdbb67af9f3b2a0d094d0
Renato Lacerda <renato.lacerda@in8.com.br>
2023-01-04 05:15:10 -0300

    fix: ajust endpoint of accident controller

--- src/controllers/accidents/accident.controller.ts
+++ src/controllers/accidents/accident.controller.ts
@@ -1,6 +1,7 @@
 import { PostgresDataSource } from "../../data-source.js";
 import { Accident } from "../../entity/Accident.js";
 import { ResponseToolkit, Request } from 'hapi'
+import { createAccident } from "../../repository/index.js";
 
 
 export const accidentController = () => {
@@ -24,13 +25,7 @@ export const accidentController = () => {
         {
             method: 'POST',
             path: '/accidents',
-            handler: ({ payload }: Request, h: ResponseToolkit, err?: Error) => {
-                const { sinister, date } = payload as Partial<Accident>;
-                const accident: Partial<Accident> = PostgresDataSource.manager.create(Accident,
-                    { sinister, date }
-                );
-                return PostgresDataSource.manager.save<Partial<Accident>>(accident);
-            }
+            handler: createAccident
         },
         {
             method: 'PATCH',
