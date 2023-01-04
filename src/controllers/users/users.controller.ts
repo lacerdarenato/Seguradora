13a990ddcb577440b44cc858e002dd384d9d51be
Renato Lacerda <renato.lacerda@in8.com.br>
2023-01-04 01:29:56 -0300

    refact: change endpoints of users controller

--- src/controllers/users/users.controller.ts
+++ src/controllers/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { PostgresDataSource } from "../../data-source.js";
 import { User } from "../../entity/User.js";
 import { ResponseToolkit, Request } from 'hapi'
+import { createClient, addThird, editClient } from '../../repository/index.js'
 
 
 export const userController = () => {
@@ -10,34 +11,29 @@ export const userController = () => {
             path: '/users',
             handler: (request: Request, h: ResponseToolkit, err?: Error) => {
                 return PostgresDataSource.manager.find(User);
-
             }
         },
         {
             method: 'GET',
             path: '/users/{id}',
-            handler: ({ params: { id } }: Request, h: ResponseToolkit, err?: Error) => {
-                return PostgresDataSource.manager.findOneBy(User, { id: Number(id) });
-
+            handler: async ({ params: { id } }: Request, h: ResponseToolkit, err?: Error) => {
+                return await PostgresDataSource.manager.findOneBy(User, { id: Number(id) });
             }
         },
         {
             method: 'POST',
-            path: '/users',
-            handler: ({ payload }: Request, h: ResponseToolkit, err?: Error) => {
-                const { cpf, firstName, lastName, phone, type } = payload as Partial<User>;
-                const user: Partial<User> = PostgresDataSource.manager.create(User,
-                    { cpf, firstName, lastName, phone, type }
-                );
-                return PostgresDataSource.manager.save<Partial<User>>(user);
-            }
+            path: '/users/client',
+            handler: createClient
         },
         {
             method: 'PATCH',
-            path: '/users/{id}',
-            handler: async ({ params: { id }, payload }: Request, h: ResponseToolkit, err?: Error) => {
-                return await PostgresDataSource.manager.update<Partial<User>>(User, id, payload)
-            }
+            path: '/users/client/{id}',
+            handler: editClient
+        },
+        {
+            method: 'POST',
+            path: '/users/third',
+            handler: addThird
         },
     ]
 }
