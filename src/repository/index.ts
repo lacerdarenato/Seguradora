3e77d3040ae330ae30ad4bbb807aa66dc4be64a8
Renato Lacerda <renato.lacerda@in8.com.br>
2023-01-04 05:15:53 -0300

    feat: add new exports to repository index

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -1,1 +1,3 @@
 export { createClient, addThird, editClient } from './users/user.repo.js'
+export { getVehiclesByUser, createVehicle } from './vehicles/vehicle.repo.js'
+export { createAccident} from './accidents/accident.repo.js'
