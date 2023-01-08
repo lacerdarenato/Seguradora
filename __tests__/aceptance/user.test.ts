8c10d48020d02955760d7fa3bc98ff1853e8f44b
Renato Lacerda <renato.lacerda@in8.com.br>
2023-01-08 15:46:00 -0300

    feat: example of unit test

--- __tests__/aceptance/user.test.ts
+++ __tests__/aceptance/user.test.ts
@@ -1,7 +1,13 @@
-import { PostgresDataSourceTest } from "../../src/data-source";
+// import { PostgresDataSourceTest } from "../../src/data-source";
 import request from 'supertest';
 import server from "../../src/server";
 
+const testuser = {
+    "cpf": "12312312000",
+    "firstName": "Renato",
+    "lastName": "Lacerda",
+    "phone": "1111",
+}
 
 // beforeAll(async () => {
 //     // await PostgresDataSourceTest.initialize()
@@ -15,4 +21,25 @@ import server from "../../src/server";
 it('shold be no user initialy', async () => {
     const response = await request(server).get('/users');
     expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual([]);
+})
+
+it('create user without CPF', async () => {
+    const response = await request(server).post('/users/client');
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual({
+        "error": "null value in column \"cpf\" of relation \"user\" violates not-null constraint"
+    });
+})
+
+it('first user successfuly created', async () => {
+    const response = await request(server).post('/users/client');
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual({
+        "msg": "Cliente cadastrado com sucesso",
+        "data": {
+            ...testuser,
+            "id": 1
+        }
+    });
 })
